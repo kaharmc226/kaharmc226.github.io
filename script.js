b14653bf0e39f926c3c7864e99718a4e74e2f641
kahar_crunchycloud <klencoc226@gmail.com>
2025-06-23 15:27:50 +0900

    this time the coord is right

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Example: Converting UTM 51S (300000, 7395000) to WGS84
+
 // Define the image bounds (southwest and northeast corners)
 var imageBounds = [
-    [460851.497, 9549904.311], // southwest
-    [438756.099, 9568134.933]  // northeast
+    [438782.557, 9549938.178], // southwest
+    [460822.393, 9568035.714]  // northeast
 ];
 
 // Add the PNG overlay
